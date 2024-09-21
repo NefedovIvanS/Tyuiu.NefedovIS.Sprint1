1dbb32dedd3bd06e5eeefe18e7f1b2da860a75dd
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-09-21 19:53:41 +0500

    Task3

---
+++ Tyuiu.NefedovIS.Sprint1.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+namespace Tyuiu.NefedovIS.Sprint1.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.NefedovIS.Sprint1.Task3.V11.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint1.Task3.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x1, y1, x2, y2, x3, y3;
+
+            Console.WriteLine("Введите значение координаты углов:");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Площадь треугольника: {ds.TriangleArea(x1, y1, x2, y2, x3, y3)} кв.см");
+
+            Console.ReadLine();
+        }
+    }
+}
