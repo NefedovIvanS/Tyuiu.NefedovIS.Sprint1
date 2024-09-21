cc17018d7ac222cf30e4801a2a9cfde38baca44e
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-09-21 22:08:14 +0500

    Task4

---
+++ Tyuiu.NefedovIS.Sprint1.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.NefedovIS.Sprint1.Task4.V27.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint1.Task4.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint1.Task4.V27/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.NefedovIS.Sprint1.Task4.V27.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint1.Task4.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"{ds.Calculate(x, y)}");
+
+            Console.ReadLine();
+        }
+    }
+}
