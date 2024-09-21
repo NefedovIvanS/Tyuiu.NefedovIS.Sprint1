ed1c96d2fdbe1282d4c8e06aa11554925ecf82ad
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-09-22 01:35:07 +0500

    Task5

---
+++ Tyuiu.NefedovIS.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.NefedovIS.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.NefedovIS.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint1.Task5.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int k;
+
+            Console.WriteLine("Введите значение k:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"{ds.Calculate(k)}");
+
+            Console.ReadLine();
+        }
+    }
+}
